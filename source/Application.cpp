7aafb619d2d34277513089040b3882f51bcb8ab4
Nicolae Christoffersen <nicochristof@gmail.com>
2026-04-24 15:20:15 -0700

    cleaned up some includes

--- source/Application.cpp
+++ source/Application.cpp
@@ -7,16 +7,6 @@
 
 #include "Renderer.h"
 
-#include "VertexArray.h"
-#include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
-#include "IndexBuffer.h"
-#include "Shader.h"
-#include "Texture.h"
-
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
